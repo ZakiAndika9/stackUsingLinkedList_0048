39ea9dbbfd1da18bc94a62f0970d7fbbd3ef4fd7
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-06-05 11:25:39 +0700

    implement pop method for Stack

--- stack.cpp
+++ stack.cpp
@@ -45,3 +45,14 @@ class Stack
 		return top == NULL;
 	}
 
+
+	void pop()
+	{
+		if (isEmpty())
+		{
+			cout << "Stack is empty." << endl;
+			return;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+	}
